e88ca5092a6342502b7fce7d786083d9e840cba6
Ubuntu <ubuntu@ip-172-31-56-74.ec2.internal>
2020-11-23 21:20:15 +0000

    Area and Perimeter with basic math

---
+++ AreaAndPerimeter.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Roman Cernetchi All rights reserved
+//
+// Created by: Roman Cernetchi
+// Created on: November 2020
+// This program calculates the Area and Perimeter of a rectangle
+//     with dimensions of 5cm x 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "if a rectangle has dimensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << (5*3) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << (2*(5+3)) << "cm" << std::endl;
+}
---
+++ BasicMath.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 Roman Cernetchi All rights reserved
+//
+// Created by: Roman Cernetchi
+// Created on: November 2020
+// This is a "Hello, World!" program, but it looks nicer
+
+#include <iostream>
+
+int main() {
+    std::cout << "9+2=" << (9+2) << std::endl;
+    std::cout << "7-2=" << (7-2) << std::endl;
+    std::cout << "4/2=" << (4/2) << std::endl;
+    std::cout << "4+4/2=" << (4+4/2) << std::endl;
+}
